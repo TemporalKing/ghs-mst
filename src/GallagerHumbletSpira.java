38237133b453eafcd92bc92f8d44df4d8c5932af
Johann <johann.meyer@gmail.com>
2017-12-01 13:22:06 +0100

    Added files to not be tracked

--- src/GallagerHumbletSpira.java
+++ src/GallagerHumbletSpira.java
@@ -4,7 +4,7 @@ public class GallagerHumbletSpira implements GallagerHumbletSpira_RMI{
 	
 	private void println(String message)
     {
-        String pidStr = "(" + this.id + ") ";
-        System.err.println(pidStr + message);
+//        String pidStr = "(" + this.id + ") ";
+//        System.err.println(pidStr + message);
     }
 }
