66c64bed948022488810c97fe4877bdab51194d7
tkolenbrander <T.J.Kolenbrander@student.tudelft.nl>
2017-12-07 10:25:43 +0100

    Connect message implemented

--- src/ConnectMessage.java
+++ src/ConnectMessage.java
@@ -1,16 +1,30 @@
 
 public class ConnectMessage implements Message {
-    private int L;
+    private int L; //L of sender
+    private int id; //id of sender
 
-    public ConnectMessage(int L) {
+    public ConnectMessage(int id, int L) {
+        this.id = id;
         this.L = L;
-    }
-    
-    public int getL() {
-        return L;
     }
 
     public void execute(GallagerHumbletSpira instance) {
-        //TODO: Logic
+        if(instance.SN == -1) {
+            instance.wakeUp();
+        }
+        
+        if(L < instance.LN) {
+            instance.neighbours_status.put(id, 1);
+            instance.sendInitiate(id, instance.LN, instance.FN, instance.SN);
+            if(instance.SN == 0) {
+                instance.find_count++;
+            }
+        } else {
+            if(instance.neighbours_status.get(id) == 0) {
+                instance.message_queue.add(this);
+            } else {
+                instance.sendInitiate(id, instance.LN + 1, instance.neighbours_weight.get(id), 0);
+            }
+        }
     }
 }
--- src/GallagerHumbletSpira.java
+++ src/GallagerHumbletSpira.java
@@ -1,19 +1,44 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class GallagerHumbletSpira implements GallagerHumbletSpira_RMI{
 
     private int id;
     
-    private int LN; //Level of the current fragment it is part of
-    private int FN; //Name of the current fragment it is part of
-    private int SN; //State of the node, 0 = find, 1 = found
-    private int in_branch; //ID of edge towards core
-    private int test_edge; //ID of edge checked whether other end in same fragment
-    private int best_edge; //ID of candidate MOE
-    private int best_weight; //Weight of candidate MOE
-    private int find_count; //Number of report messages expected
+    protected int LN; //Level of the current fragment it is part of
+    protected int FN; //Name of the current fragment it is part of
+    protected int SN; //State of the node,-1 = sleeping, 0 = find, 1 = found
+    protected int in_branch; //ID of edge towards core
+    protected int test_edge; //ID of edge checked whether other end in same fragment
+    protected int best_edge; //ID of candidate MOE
+    protected int best_weight; //Weight of candidate MOE
+    protected int find_count; //Number of report messages expected
+    
+    protected Queue<Message> message_queue; //Message queue
+    
+    protected HashMap<Integer,Integer> neighbours_status; //Keeps track of neighbours
+    //-1 = not in MST, 0 = unknown, 1 = in MST
+    
+    protected final HashMap<Integer,Integer> neighbours_weight;
 	
+    public GallagerHumbletSpira(int id) {
+        this.id = id;
+        this.neighbours_status = new HashMap<Integer,Integer>();
+        this.message_queue = new LinkedList<Message>();
+        this.neighbours_weight = new HashMap<Integer,Integer>();
+    }
+    
     public void receiveMessage(Message m) {
         m.execute(this);
+    }
+
+    public void wakeUp() {
+        //TODO
+    }
+    
+    public void sendInitiate(int receiveID, int level, int name, int state) {
+        //TODO
     }
     
     private void println(String message)
